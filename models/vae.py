d604e94b4db8fafcd534fa1ea9726c80ead6d9c4
Wilson Yan <wilson1.yan@berkeley.edu>
2019-04-12 15:21:50 -0700

    new vae

--- models/vae.py
+++ models/vae.py
@@ -7,6 +7,41 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+class ResidualBlock(nn.Module):
+
+    def __init__(self, in_channels, n_filters):
+        super(ResidualBlock, self).__init__()
+        self.in_channels = in_channels
+        self.n_filters = n_filters
+
+        self.model = []
+        self.model.append(nn.ReLU())
+        self.model.append(nn.Conv2d(in_channels, n_filters, 3, padding=1))
+        self.model.append(nn.ReLU())
+        self.model.append(nn.Conv2d(n_filters, n_filters, 1))
+        self.model.append(nn.ReLU())
+        self.model.append(nn.Conv2d(n_filters, in_channels, 3, padding=1))
+
+        self.model = nn.Sequential(*self.model)
+
+    def forward(self, x):
+        out = self.model(x)
+        return out + x
+
+class ResidualStack(nn.Module):
+    def __init__(self, in_channels, n_filters=64, n_blocks=5, tail=False):
+        super(ResidualStack, self).__init__()
+        self.n_blocks = n_blocks
+        self.model = []
+        for _ in range(n_blocks):
+            self.model.append(ResidualBlock(in_channels, n_filters))
+        if not tail:
+            self.model.append(nn.ReLU())
+        self.model = nn.Sequential(*self.model)
+
+    def forward(self, x):
+        return self.model(x)
+
 class Decoder(nn.Module):
     """ VAE decoder """
     def __init__(self, img_channels, latent_size):
@@ -20,13 +55,19 @@ class Decoder(nn.Module):
         self.deconv3 = nn.ConvTranspose2d(64, 32, 6, stride=2)
         self.deconv4 = nn.ConvTranspose2d(32, img_channels, 6, stride=2)
 
+        self.conv_layers = nn.Sequential(
+            ResidualStack(128, n_blocks=2),
+            nn.ReLU(),
+            nn.ConvTranspose2d(128, 128, 4, stride=2, padding=1)
+            nn.ConvTranspose2d(128, 128, 4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(128, 6, 4, stride=2, padding=1)
+        )
+
     def forward(self, x): # pylint: disable=arguments-differ
         x = F.relu(self.fc1(x))
-        x = x.unsqueeze(-1).unsqueeze(-1)
-        x = F.relu(self.deconv1(x))
-        x = F.relu(self.deconv2(x))
-        x = F.relu(self.deconv3(x))
-        reconstruction = F.sigmoid(self.deconv4(x))
+        x = x.view(x.size(0), 64, 4, 4)
+        reconstruction = F.sigmoid(self.conv_layers(x))
         return reconstruction
 
 class Encoder(nn.Module): # pylint: disable=too-many-instance-attributes
@@ -34,23 +75,24 @@ class Encoder(nn.Module): # pylint: disable=too-many-instance-attributes
     def __init__(self, img_channels, latent_size):
         super(Encoder, self).__init__()
         self.latent_size = latent_size
-        #self.img_size = img_size
         self.img_channels = img_channels
 
-        self.conv1 = nn.Conv2d(img_channels, 32, 4, stride=2)
-        self.conv2 = nn.Conv2d(32, 64, 4, stride=2)
-        self.conv3 = nn.Conv2d(64, 128, 4, stride=2)
-        self.conv4 = nn.Conv2d(128, 256, 2, stride=2)
+        self.conv_layers = nn.Sequential(
+            nn.Conv2d(img_channels, 128, 4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, 4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, 4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, 3, padding=1),
+            ResidualStack(128, n_blocks=2, tail=True)
+        )
 
-        self.fc_mu = nn.Linear(2*2*256, latent_size)
+        self.fc_mu = nn.Linear(4*4*128, latent_size)
         self.fc_logsigma = nn.Linear(2*2*256, latent_size)
 
-
     def forward(self, x): # pylint: disable=arguments-differ
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
-        x = F.relu(self.conv4(x))
+        x = self.conv_layers(x)
         x = x.view(x.size(0), -1)
 
         mu = self.fc_mu(x)
