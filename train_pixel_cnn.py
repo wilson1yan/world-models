d70f9aa768b229fce2d99e6df35e5e113f8a4009
Wilson Yan <wilson1.yan@berkeley.edu>
2019-04-15 21:52:16 -0700

    Pixel CNN training script

---
+++ train_pixel_cnn.py
@@ -0,0 +1,177 @@
+""" Training VAE """
+import argparse
+from os.path import join, exists
+from os import mkdir
+
+import numpy as np
+
+import torch
+import torch.utils.data
+from torch import optim
+from torch.nn import functional as F
+from torchvision import transforms
+from torchvision.utils import save_image
+
+from models.pixel_cnn import PixelCNN
+
+from utils.misc import save_checkpoint
+from utils.misc import LSIZE, RED_SIZE
+## WARNING : THIS SHOULD BE REPLACE WITH PYTORCH 0.5
+from utils.learning import EarlyStopping
+from utils.learning import ReduceLROnPlateau
+from data.loaders import RolloutObservationDataset
+
+parser = argparse.ArgumentParser(description='PixelCNN Trainer')
+parser.add_argument('--batch-size', type=int, default=32, metavar='N',
+                    help='input batch size for training (default: 32)')
+parser.add_argument('--epochs', type=int, default=1000, metavar='N',
+                    help='number of epochs to train (default: 1000)')
+parser.add_argument('--logdir', type=str, help='Directory where results are logged')
+parser.add_argument('--noreload', action='store_true',
+                    help='Best model is not reloaded if specified')
+parser.add_argument('--nosamples', action='store_true',
+                    help='Does not save samples during training if specified')
+
+
+args = parser.parse_args()
+cuda = torch.cuda.is_available()
+
+N_COLOR_DIM = 4
+
+torch.manual_seed(123)
+# Fix numeric divergence due to bug in Cudnn
+torch.backends.cudnn.benchmark = True
+
+device = torch.device("cuda" if cuda else "cpu")
+
+
+transform_train = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.Resize((RED_SIZE, RED_SIZE)),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+])
+
+transform_test = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.Resize((RED_SIZE, RED_SIZE)),
+    transforms.ToTensor(),
+])
+
+dataset_train = RolloutObservationDataset('datasets/carracing',
+                                          transform_train, train=True)
+dataset_test = RolloutObservationDataset('datasets/carracing',
+                                         transform_test, train=False)
+train_loader = torch.utils.data.DataLoader(
+    dataset_train, batch_size=args.batch_size, shuffle=True, num_workers=2)
+test_loader = torch.utils.data.DataLoader(
+    dataset_test, batch_size=args.batch_size, shuffle=True, num_workers=2)
+
+model = PixelCNN(3, N_COLOR_DIM, n_blocks=12, cond=False).to(device)
+
+optimizer = optim.Adam(model.parameters(), lr=np.sqrt(args.batch_size / 32) * 1e-3)
+scheduler = ReduceLROnPlateau(optimizer, 'min', factor=0.5, patience=5)
+earlystopping = EarlyStopping('min', patience=30)
+
+# Reconstruction + KL divergence losses summed over all elements and batch
+def loss_function(recon_x, x):
+    """ Cross-Entropy loss function """
+    target = (x * (N_COLOR_DIM - 1)).long()
+    loss = F.cross_entropy(recon_x, target)
+
+    return loss
+
+
+def train(epoch):
+    """ One training epoch """
+    model.train()
+    dataset_train.load_next_buffer()
+    train_loss = 0
+    for batch_idx, data in enumerate(train_loader):
+        data = data.to(device)
+        optimizer.zero_grad()
+        recon_batch = model(data)
+        loss = loss_function(recon_batch, data)
+        loss.backward()
+        train_loss += loss.item()
+        optimizer.step()
+        if batch_idx % 20 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader),
+                loss.item() / len(data)))
+
+    print('====> Epoch: {} Average loss: {:.4f}'.format(
+        epoch, train_loss / len(train_loader.dataset)))
+
+
+def test():
+    """ One test epoch """
+    model.eval()
+    dataset_test.load_next_buffer()
+    test_loss = 0
+    with torch.no_grad():
+        for data in test_loader:
+            data = data.to(device)
+            recon_batch = model(data)
+            test_loss += loss_function(recon_batch, data).item()
+
+    test_loss /= len(test_loader.dataset)
+    print('====> Test set loss: {:.4f}'.format(test_loss))
+    return test_loss
+
+# check vae dir exists, if not, create it
+vae_dir = join(args.logdir, 'pixel_cnn')
+if not exists(vae_dir):
+    mkdir(vae_dir)
+    mkdir(join(vae_dir, 'samples'))
+
+reload_file = join(vae_dir, 'best.tar')
+if not args.noreload and exists(reload_file):
+    state = torch.load(reload_file)
+    print("Reloading model at epoch {}"
+          ", with test error {}".format(
+              state['epoch'],
+              state['precision']))
+    model.load_state_dict(state['state_dict'])
+    optimizer.load_state_dict(state['optimizer'])
+    scheduler.load_state_dict(state['scheduler'])
+    earlystopping.load_state_dict(state['earlystopping'])
+
+
+cur_best = None
+
+for epoch in range(1, args.epochs + 1):
+    train(epoch)
+    test_loss = test()
+    scheduler.step(test_loss)
+    earlystopping.step(test_loss)
+
+    # checkpointing
+    best_filename = join(vae_dir, 'best.tar')
+    filename = join(vae_dir, 'checkpoint.tar')
+    is_best = not cur_best or test_loss < cur_best
+    if is_best:
+        cur_best = test_loss
+
+    save_checkpoint({
+        'epoch': epoch,
+        'state_dict': model.state_dict(),
+        'precision': test_loss,
+        'optimizer': optimizer.state_dict(),
+        'scheduler': scheduler.state_dict(),
+        'earlystopping': earlystopping.state_dict()
+    }, is_best, filename, best_filename)
+
+
+
+    if not args.nosamples:
+        print("Sampling")
+        with torch.no_grad():
+            sample = model.sample().cpu()
+            save_image(sample,
+                       join(vae_dir, 'samples/sample_' + str(epoch) + '.png'))
+
+    if earlystopping.stop:
+        print("End of Training because of early stopping at epoch {}".format(epoch))
+        break
