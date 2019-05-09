56802e3ae3fc5d8a48589f180ab890a1603cf380
Wilson Yan <wilson1.yan@berkeley.edu>
2019-05-09 14:52:17 -0700

    cat rnn

---
+++ models/rnn_cat.py
@@ -0,0 +1,66 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import numpy as np
+
+def cat_loss(latent_next_obs, dist_outs):
+    print(dist_outs.size(), latent_next_obs.size())
+    loss = F.cross_entropy(dist_outs, latent_next_obs, reduce=False)
+    loss = loss.view(loss.size(0), -1)
+    loss = loss.sum(-1).mean(0)
+    return loss
+
+class RNNCat(nn.Module):
+
+    def __init__(self, latent_shape, code_dim, K, actions, hiddens):
+        super(RNNCat, self).__init__()
+        lsize = np.prod(latent_shape) * code_dim
+        self.rnn = nn.LSTM(lsize + actions, hiddens)
+        self.output_layer = nn.Linear(hiddens, np.prod(latent_shape) * K + 2)
+
+        self.code_dim = code_dim
+        self.latent_shape = latent_shape
+        self.K = K
+        self.n_latents = np.prod(latent_shape)
+
+    def forward(self, actions, latents):
+        seq_len, bs = actions.size(0), action.size(1)
+        latents_flat = latents.view(latents.size(0), latents.size(1), -1)
+
+        ins = torch.cat([actions, latent])
+        outs, _ = self.rnn(ins)
+        outs = self.output_layer(outs)
+        dist_outs = [:, :, :-2]
+        dist_outs = dist_outs.view(dist_outs.size(0), dist_outs.size(1), self.n_latents, self.K)
+        dist_outs = dist_outs.permute(1, 3, 0, 2) # B x K x SEQ x N_LATENTS
+
+        rs = outs[:, :, -2]
+        ds = outs[:, :, -1]
+
+        return dist_outs, rs, ds
+
+
+class RNNCatCell(nn.Module):
+
+    def __init__(self, latent_shape, code_dim, K, actions, hiddens):
+        super(RNNCatCell, self).__init__()
+        self.latent_shape = latent_shape
+        self.K = K
+        self.n_latents = np.prod(latent_shape)
+
+        lsize = self.n_latents * code_dim
+
+        self.rnn = nn.LSTMCell(lsize + actions, hiddens)
+        self.output_layer = nn.Linear(hiddens, self.n_latents * K)
+
+
+    def forward(self, action, latent, hidden):
+        in_al = torch.cat([action, latent], dim=1)
+
+        next_hidden = self.rnn(in_al, hidden)
+        out_rnn = next_hidden[0]
+
+        dist_out = self.output_layer(out_rnn)
+        dist_out = dist_out.view(dist_out.size(0), self.n_latents, K)
+        return dist_out.permute(0, 2, 1)
---
+++ train_vqvae_rnn.py
@@ -0,0 +1,232 @@
+""" Recurrent model training """
+import argparse
+from functools import partial
+from os.path import join, exists
+from os import makedirs
+import torch
+import torch.nn.functional as f
+from torch.utils.data import DataLoader
+from torchvision import transforms
+import numpy as np
+from tqdm import tqdm
+from models.rnn_cat import RNNCat, RNNCatCell, cat_loss
+from utils.misc import save_checkpoint
+from utils.misc import ASIZE, LSIZE, RSIZE, RED_SIZE, SIZE, IncreaseSize, N_COLOR_DIM
+from utils.learning import EarlyStopping
+## WARNING : THIS SHOULD BE REPLACED WITH PYTORCH 0.5
+from utils.learning import ReduceLROnPlateau
+
+from data.loaders import RolloutSequenceDataset
+from torchvision.utils import save_image
+
+parser = argparse.ArgumentParser("RNNCat training")
+parser.add_argument('--logdir', type=str, default='logs',
+                    help="Where things are logged and models are loaded from.")
+parser.add_argument('--noreload', action='store_true',
+                    help="Do not reload if specified.")
+parser.add_argument('--include_reward', action='store_true',
+                    help="Add a reward modelisation term to the loss.")
+parser.add_argument('--dataset', type=str, default='carracing')
+args = parser.parse_args()
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# constants
+BSIZE = 16
+SEQ_LEN = 32
+epochs = 1000
+
+# Loading VAE
+vae_dir = join(args.logdir, args.dataset, 'vqvae')
+vae_file = join(vae_dir, 'best.tar')
+assert exists(vae_file), "No trained VAE in the logdir..."
+state = torch.load(vae_file)
+print("Loading VAE at epoch {} "
+      "with test error {}".format(
+          state['epoch'], state['precision']))
+
+vae = torch.load(join(vae_dir, 'model_best.pt')).to(device)
+
+# Loading model
+rnn_dir = join(args.logdir, args.dataset, 'cat_rnn')
+rnn_file = join(rnn_dir, 'best.tar')
+
+if not exists(rnn_dir):
+    makedirs(rnn_dir)
+
+mdrnn = RNNCat((4, 4), 64, 128, ASIZE, RSIZE).to(device)
+optimizer = torch.optim.RMSprop(mdrnn.parameters(), lr=1e-3, alpha=.9)
+scheduler = ReduceLROnPlateau(optimizer, 'min', factor=0.5, patience=5)
+earlystopping = EarlyStopping('min', patience=30)
+
+if exists(rnn_file) and not args.noreload:
+    rnn_state = torch.load(rnn_file)
+    print("Loading MDRNN at epoch {} "
+          "with test error {}".format(
+              rnn_state["epoch"], rnn_state["precision"]))
+    tmp = torch.load(join(rnn_dir, 'model_best.pt'))
+    mdrnn.load_state_dict({k+'_l0': v for k, v in tmp.state_dict().items()})
+    optimizer.load_state_dict(rnn_state["optimizer"])
+    scheduler.load_state_dict(state['scheduler'])
+    earlystopping.load_state_dict(state['earlystopping'])
+
+
+
+# Data Loading
+# transform = transforms.Lambda(
+#     lambda x: np.transpose(x, (0, 3, 1, 2)) / 255)
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    IncreaseSize(game=args.dataset, n_expand=2),
+    transforms.ToPILImage(),
+    transforms.Resize((RED_SIZE, RED_SIZE)),
+    transforms.ToTensor(),
+])
+
+train_loader = DataLoader(
+    RolloutSequenceDataset(join('datasets', args.dataset), SEQ_LEN,
+                           transform, buffer_size=30),
+    batch_size=BSIZE, num_workers=8, shuffle=True)
+test_loader = DataLoader(
+    RolloutSequenceDataset(join('datasets', args.dataset), SEQ_LEN,
+                           transform, train=False, buffer_size=10),
+    batch_size=BSIZE, num_workers=8)
+
+def to_latent(obs, next_obs):
+    """ Transform observations to latent space.
+
+    :args obs: 5D torch tensor (BSIZE, SEQ_LEN, ASIZE, SIZE, SIZE)
+    :args next_obs: 5D torch tensor (BSIZE, SEQ_LEN, ASIZE, SIZE, SIZE)
+
+    :returns: (latent_obs, latent_next_obs)
+        - latent_obs: 4D torch tensor (BSIZE, SEQ_LEN, LSIZE)
+        - next_latent_obs: 4D torch tensor (BSIZE, SEQ_LEN, LSIZE)
+    """
+    with torch.no_grad():
+        batch_size, seq_len = obs.size(0), obs.size(1)
+        obs = obs.view(batch_size * seq_len, *obs.size()[2:])
+        next_obs = next_obs.view(batch_size * seq_len, *next_obs.size()[2:])
+
+        latent_obs = vae.encode(obs)
+        latent_next_obs = vae.encode(next_obs)
+
+        latent_obs = latent_obs.view(batch_size, seq_len, -1)
+        latent_next_obs = latent_next_obs.view(batch_size, seq_len, -1)
+
+    return latent_obs, latent_next_obs
+
+def get_loss(latent_obs, action, reward, terminal,
+             latent_next_obs, include_reward: bool):
+    """ Compute losses.
+
+    The loss that is computed is:
+    (GMMLoss(latent_next_obs, GMMPredicted) + MSE(reward, predicted_reward) +
+         BCE(terminal, logit_terminal)) / (LSIZE + 2)
+    The LSIZE + 2 factor is here to counteract the fact that the GMMLoss scales
+    approximately linearily with LSIZE. All losses are averaged both on the
+    batch and the sequence dimensions (the two first dimensions).
+
+    :args latent_obs: (BSIZE, SEQ_LEN, LSIZE) torch tensor
+    :args action: (BSIZE, SEQ_LEN, ASIZE) torch tensor
+    :args reward: (BSIZE, SEQ_LEN) torch tensor
+    :args latent_next_obs: (BSIZE, SEQ_LEN, LSIZE) torch tensor
+
+    :returns: dictionary of losses, containing the gmm, the mse, the bce and
+        the averaged loss.
+    """
+    latent_obs, action,\
+        reward, terminal  = [arr.transpose(1, 0)
+                             for arr in [latent_obs, action,
+                                       reward, terminal]]
+    dist_outs, rs, ds = mdrnn(action, latent_obs)
+    gmm = cat_loss(latent_next_obs, dist_outs)
+    bce = f.binary_cross_entropy_with_logits(ds, terminal)
+    if include_reward:
+        mse = f.mse_loss(rs, reward)
+        scale = LSIZE + 2
+    else:
+        mse = 0
+        scale = LSIZE + 1
+    loss = (gmm + bce + mse) / scale
+    return dict(gmm=gmm, bce=bce, mse=mse, loss=loss)
+
+def process(data):
+    data *= 255
+    return torch.floor(data / (2 ** 8 / N_COLOR_DIM)) / (N_COLOR_DIM - 1)
+
+def data_pass(epoch, train, include_reward): # pylint: disable=too-many-locals
+    """ One pass through the data """
+    if train:
+        mdrnn.train()
+        loader = train_loader
+    else:
+        mdrnn.eval()
+        loader = test_loader
+
+    loader.dataset.load_next_buffer()
+
+    cum_loss = 0
+    cum_gmm = 0
+    cum_bce = 0
+    cum_mse = 0
+
+    pbar = tqdm(total=len(loader.dataset), desc="Epoch {}".format(epoch))
+    for i, data in enumerate(loader):
+        obs, action, reward, terminal, next_obs = [arr.to(device) for arr in data]
+        obs, next_obs = process(obs), process(next_obs)
+
+        # transform obs
+        latent_obs, latent_next_obs = to_latent(obs, next_obs)
+
+        if train:
+            losses = get_loss(latent_obs, action, reward,
+                              terminal, latent_next_obs, include_reward)
+
+            optimizer.zero_grad()
+            losses['loss'].backward()
+            optimizer.step()
+        else:
+            with torch.no_grad():
+                losses = get_loss(latent_obs, action, reward,
+                                  terminal, latent_next_obs, include_reward)
+
+        cum_loss += losses['loss'].item()
+        cum_gmm += losses['gmm'].item()
+        cum_bce += losses['bce'].item()
+        cum_mse += losses['mse'].item() if hasattr(losses['mse'], 'item') else \
+            losses['mse']
+
+        pbar.set_postfix_str("loss={loss:10.6f} bce={bce:10.6f} "
+                             "gmm={gmm:10.6f} mse={mse:10.6f}".format(
+                                 loss=cum_loss / (i + 1), bce=cum_bce / (i + 1),
+                                 gmm=cum_gmm / LSIZE / (i + 1), mse=cum_mse / (i + 1)))
+        pbar.update(BSIZE)
+    pbar.close()
+    return cum_loss * BSIZE / len(loader.dataset)
+
+
+train = partial(data_pass, train=True, include_reward=args.include_reward)
+test = partial(data_pass, train=False, include_reward=args.include_reward)
+
+cur_best = None
+for e in range(epochs):
+    train(e)
+    test_loss = test(e)
+    scheduler.step(test_loss)
+    earlystopping.step(test_loss)
+
+    is_best = not cur_best or test_loss < cur_best
+    if is_best:
+        cur_best = test_loss
+    tmp = RNNCat((4, 4), 64, 128, ASIZE, RSIZE)
+    tmp.load_state_dict({k.strip('_l0'): v for k, v in mdrnn.state_dict().items()})
+    save_checkpoint({
+        "optimizer": optimizer.state_dict(),
+        'scheduler': scheduler.state_dict(),
+        'earlystopping': earlystopping.state_dict(),
+        "precision": test_loss,
+        "epoch": e}, tmp, is_best, rnn_dir)
+
+    if earlystopping.stop:
+        print("End of Training because of early stopping at epoch {}".format(e))
+        break
